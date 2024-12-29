90d736c59f109e9a5ccc2be75a013016aad6b086
Shaiful191 <saifulislamridoy1999@gmail.com>
2024-12-29 22:33:57 +0600

    asychronous programing added in custom hook when data fetching

--- src/CustomHooks/useFetch.jsx
+++ src/CustomHooks/useFetch.jsx
@@ -1,41 +1,43 @@
 import { useEffect, useState } from 'react'
+
 const useFetch = (url) => {
 
   const [data, setData] = useState(null);
   const [isLoaded, setIsLoaded] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
 
+
+  const fetchData = async (abortController) => {
+    try {
+      const res = await fetch(url, { signal: abortController.signal });
+      if (!res.ok) {
+        throw new Error("Could not fetch the data for that resource");
+      }
+      const data = await res.json(); // Parse JSON response
+      setData(data);
+      setIsLoaded(false);
+      setError(null);
+    } catch (err) {
+      if (err.name === 'AbortError') {
+        console.log('fetch aborted');
+      } else {
+        setIsLoaded(false);
+        setError(err.message);
+      }
+    }
+  };
+
+
+  useEffect(() => {
     const abortController = new AbortController();
 
-    fetch(url, { signal: abortController.signal })
-      .then(res => {
-        if (!res.ok) {
-          throw new Error("Could not fetch the data for that resource");
-        }
-        return res.json(); // Parse JSON response
-      })
-      .then(data => {
-        // console.log(data);
-        setData(data);
-        setIsLoaded(false);
-        setError(null);
-      })
-      .catch(err => {
-        if (err.name === 'AbortError') {
-          console.log('fetch aborted');
-        } else {
-          setIsLoaded(false);
-          setError(err.message);
-        }
-      });
+    fetchData(abortController);
 
-    //cleanUP
+    // Clean-up function
     return () => {
       abortController.abort();
     };
-
 
   }, [url]);
 
